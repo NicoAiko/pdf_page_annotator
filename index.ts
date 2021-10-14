689b02c9ac73ede4b5c15c3717e6e0a7d4e93553
NicoAiko <n.linnemann@nicoaiko.de>
2021-10-14 20:53:41 +0200

    First commit

---
+++ index.ts
@@ -0,0 +1,73 @@
+import yargs = require('yargs/yargs');
+import { PDFDocument, rgb, StandardFonts } from 'pdf-lib';
+import FS from 'fs';
+
+interface Arguments {
+  [x: string]: unknown;
+  i: string;
+  o: string;
+  _: (string | number)[];
+}
+
+async function main() {
+  const argv: Arguments = await yargs(process.argv.slice(2))
+    .options({
+      i: { type: 'string', demandOption: true, alias: 'input', description: 'Input PDF file name' },
+      o: { type: 'string', demandOption: true, alias: 'output', description: 'Output PDF file name' },
+    })
+    .usage('Usage: $0 [options]')
+    .example('$0 -i input.pdf -o output.pdf', 'Read a PDF file do magic and save as the output file')
+    .help('h')
+    .alias('h', 'help')
+    .epilog('Copyright 2021 - NicoAiko')
+    .argv;
+
+
+  if (!FS.existsSync(argv.i)) {
+    console.error('The input file does not exist!');
+
+    process.exit(1);
+  }
+
+  console.log('Loading file...');
+  const inputFile = FS.readFileSync(argv.i);
+
+  console.log('Parsing...');
+  const doc = await PDFDocument.load(inputFile);
+
+  console.log('Preparing font...');
+  const helveticaFont = await doc.embedFont(StandardFonts.Helvetica);
+
+  console.log('Reading amount of pages...');
+  const pages = doc.getPages();
+  console.log(`Found ${pages.length} page${pages.length === 1 ? '' : 's'}`);
+
+  let pageNumber = 1;
+  for (const page of pages) {
+    const { width } = page.getSize();
+    const numberText = `Page ${pageNumber.toString().padStart(3, '0')}`;
+    const textWidth = helveticaFont.widthOfTextAtSize(numberText, 12);
+    const textHeight = helveticaFont.heightAtSize(12);
+    const x = width - 20 - textWidth;
+    const y = 20 + textHeight;
+
+    console.log(`Annotating ${numberText}...`);
+
+    page.drawText(numberText, {
+      x,
+      y,
+      size: 12,
+      font: helveticaFont,
+      color: rgb(1, 0, 0),
+    });
+
+    pageNumber++;
+  }
+
+  console.log('Saving to output...');
+  const pdfBytes = await doc.save();
+
+  FS.writeFileSync(argv.o, pdfBytes);
+}
+
+main();
