032679dabe4601c7f9bcfb80c57d3b580742f17e
NicoAiko <n.linnemann@nicoaiko.de>
2021-10-14 21:25:14 +0200

    Add start and end offset for annotation

--- index.ts
+++ index.ts
@@ -6,6 +6,8 @@ interface Arguments {
   [x: string]: unknown;
   i: string;
   o: string;
+  s?: number;
+  e?: number;
   _: (string | number)[];
 }
 
@@ -14,9 +16,12 @@ async function main() {
     .options({
       i: { type: 'string', demandOption: true, alias: 'input', description: 'Input PDF file name' },
       o: { type: 'string', demandOption: true, alias: 'output', description: 'Output PDF file name' },
+      s: { type: 'number', demandOption: false, alias: 'start-page', description: 'Page of the PDF where counting should start (inclusive!). If omitted, the first page of the PDF is used.' },
+      e: { type: 'number', demandOption: false, alias: 'end-page', description: 'Page of the PDF where counting should end (inclusive!). If omitted, the last page of the PDF is used.' },
     })
     .usage('Usage: $0 [options]')
     .example('$0 -i input.pdf -o output.pdf', 'Read a PDF file do magic and save as the output file')
+    .example('$0 -i input.pdf -o output.pdf -s 3 -e 10', 'Read, do magic, write. Also start with Page 3 and stop at Page 10!')
     .help('h')
     .alias('h', 'help')
     .epilog('Copyright 2021 - NicoAiko')
@@ -25,6 +30,35 @@ async function main() {
 
   if (!FS.existsSync(argv.i)) {
     console.error('The input file does not exist!');
+
+    process.exit(1);
+  }
+
+  if (FS.existsSync(argv.o)) {
+    // Check if file is writable
+    try {
+      FS.openSync(argv.o, 'r+');
+    } catch (error) {
+      console.error('The output file already exists and can\'t currently be written to. Is it opened somewhere?');
+
+      process.exit(1);
+    }
+  }
+
+  if (argv.s !== undefined && argv.s < 1) {
+    console.error('The start page must not be smaller than 1!');
+
+    process.exit(1);
+  }
+
+  if (argv.e !== undefined && argv.e < 1) {
+    console.error('The end page must not be smaller than 1!');
+
+    process.exit(1);
+  }
+
+  if ((argv.s !== undefined && argv.e !== undefined) && argv.e < argv.s) {
+    console.error('The end page must not be smaller than the start page!');
 
     process.exit(1);
   }
@@ -42,8 +76,34 @@ async function main() {
   const pages = doc.getPages();
   console.log(`Found ${pages.length} page${pages.length === 1 ? '' : 's'}`);
 
+  if (argv.s !== undefined && argv.s > pages.length) {
+    console.error('The start page is higher than available pages in the input PDF!');
+
+    process.exit(1);
+  }
+
+  if (argv.e !== undefined && argv.e > pages.length) {
+    console.warn('The end page is higher than available pages in the input PDF. End page offset is ignored!');
+  }
+
+  const startOffset = argv.s ?? 1;
+  const endOffset = argv.e ?? pages.length;
+  let pageIndex = 1;
   let pageNumber = 1;
+
+  console.log(`Annotating starts at page ${startOffset} and stops at page ${endOffset}.`);
+
   for (const page of pages) {
+    // Outside of offset
+    if (pageIndex < startOffset) {
+      pageIndex++;
+      continue;
+    }
+
+    if (pageIndex > endOffset) {
+      break;
+    }
+
     const { width } = page.getSize();
     const numberText = `Page ${pageNumber.toString().padStart(3, '0')}`;
     const textWidth = helveticaFont.widthOfTextAtSize(numberText, 12);
@@ -62,6 +122,7 @@ async function main() {
     });
 
     pageNumber++;
+    pageIndex++;
   }
 
   console.log('Saving to output...');
